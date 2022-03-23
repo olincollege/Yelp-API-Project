5f417fee8b1b831ee366e8c806c438cc00bea24f
acodringtonolin <acodrington@olin.edu>
2022-03-23 01:18:56 -0400

    need to pivot

--- API_symptoms.py
+++ API_symptoms.py
@@ -1,3 +1,11 @@
 import requests
+import API_setup
+import hashlib
+import hmac
 
-use = requests.get() 
+
+host_API = "https://sandbox-authservice.priaid.ch/login"
+
+my_hmac = hmac.new(api_key, [], hashlib.md5)
+
+print("hope this works " + str(my_hmac_cpy.digest()))
