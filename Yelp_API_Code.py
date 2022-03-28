0b57bb9d059bf8ac998e461f627b86115bc3ae65
acodringtonolin <acodrington@olin.edu>
2022-03-27 22:28:49 -0400

    updated, can get data now

--- Yelp_API_Code.py
+++ Yelp_API_Code.py
@@ -1,9 +1,6 @@
 """"
-Program designed to use Yelp API in order to...
-
-In the creation of this code 
-https://github.com/Yelp/yelp-fusion/blob/master/fusion/python/sample.py
-was used as reference for how to utilize Yelp API
+Program designed to use Yelp API in order to identify buisnesses in
+different demographical areas and comparing reviews
 
 """
 
@@ -21,26 +18,42 @@ from API_setup import client_id
 #set up of API key and required variables
 # decided to put actual API and Client key in different file for protection 
 
-API_key = api_key
-client_id = client_id
+API_KEY = api_key
+CLIENT_ID = client_id
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
 
+#this endpoint allows a max of 1000 buisnesses to be returned
+#when using specific search parameters
+ENDPOINT = "https://api.yelp.com/v3/businesses/search"
 
-#creating function to request info from yelp API
-def yelp_request(host, path, API_key):
-    """
-    Args:
-        host (str): The domain host of the API.
-        path (str): The path of the API after the domain.
-        API_key (str): the API key
-
-
-    Returns:
-
-    
-    
-    """
+#API_AUTH is a header that allows us to pass through our API key
+#this allows us be authenticated and use the API
+API_AUTH = {'Authorization': 'bearer %s' % API_KEY}
 
 
 
+#searching buisnesses based on these parameters
+PARAMETERS = {'location':'New York',
+                'limit':20,#limits to 20 searches
+                'radius':1000,#radius is 
+                'term':'McDonalds'}#optional term like coffee
+
+
+#this line requests buisnesses fitting out parameters, from specified endpoint
+#this will return as a str but in the j form
+response = requests.get(url=ENDPOINT, 
+                        params=PARAMETERS, 
+                        headers=API_AUTH)
+
+#need this line to convert out of j form
+yelp_data = response.json()  
+
+# printing data
+print(json.dumps(yelp_data, indent = 2))
+
+
+#now that data can be successfully gathered I just need to webscrape 
+#for demographically different areas then input that as location
+#then I use the API review search to see if different demographics 
+#truly do effect reviews/how good an establishment is
